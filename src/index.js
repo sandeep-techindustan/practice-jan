a0f2abbe7a5d25c0021bc977d5d3383a95f49a80
Sandeep <sandeep.techindustan@gmail.com>
2018-01-16 18:18:37 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './containers/App';
+
+export default class Index extends React.Component {
+	render() {
+		return (
+			<div>
+				<App />
+			</div>
+			)
+	}
+}
